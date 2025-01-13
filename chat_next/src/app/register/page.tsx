9b3cef54d96f0eab72b61262b9ee2915a72d62d1
adam <adamducken@gmail.com>
2025-01-13 12:25:34 +0200

    feat(next): register page

---
+++ chat_next/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '@/components/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <div className="container mx-auto flex items-center justify-center min-h-screen">
+      <RegisterForm />
+    </div>
+  )
+}
+
+
