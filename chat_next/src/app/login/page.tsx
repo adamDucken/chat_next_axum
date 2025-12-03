77545a56e69356a5f8683ad63ab455f02cafa435
adam <adamducken@gmail.com>
2025-12-03 10:46:37 +0200

    feat(next): added login page

--- chat_next/src/app/login/page.tsx
+++ chat_next/src/app/login/page.tsx
@@ -1,6 +1,15 @@
 import LoginForm from '@/components/LoginForm'
+import { checkAuthStatus } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+
+export default async function LoginPage() {
+  const isAuthenticated = await checkAuthStatus();
+
+  if (isAuthenticated) {
+    redirect('/chat'); // or in other scenario we could redirect to home or smth like that 
+  }
+
   return (
     <div className="container mx-auto flex items-center justify-center min-h-screen">
       <LoginForm />
